d544ddabd0727dd37a8892e34d4083dc893799af
Mahmoud sharifi <113373073+mahmoudsharifi@users.noreply.github.com>
2023-05-22 23:38:36 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const url = require('url')
+const { readFileSync, writeFileSync } = require('fs')
+const { join } = require('path')
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    let addr = request.url
+    let { path } = url.parse(addr, true)
+    
+    if (path.includes('documentation')) {
+        var fileName = 'documentation.html'
+    } else {
+        var fileName = 'index.html'
+    }
+    
+    let filePath = join(__dirname, fileName)
+    let file = readFileSync(filePath)
+
+    let date = new Date()
+    let timestamp = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()} - ${date.getHours()}:${date.getMinutes()}`
+    updateLog(`${filePath} / ${timestamp} \n`)
+
+    response.write(file)
+    response.end()
+})
+
+function updateLog(str) {
+    writeFileSync(join(__dirname, 'log.txt'), str, {
+        encoding: 'utf-8',
+        flag: 'a'
+    })
+}
+
+server.listen(8080);
+
+// node --watch server.js
---
+++ test.js
@@ -0,0 +1,5 @@
+
+console.log('hello node')
+console.log('Goodbye')
+
+// touch documentation.html
